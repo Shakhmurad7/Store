42a8cd2fe25ee4350d584a2e970f1f128a67be55
Shakhmurad <muradb356@gmail.com>
2024-01-09 15:09:31 +0400

    add react icon

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # React + TypeScript + Vite
 
+figma - https://www.figma.com/file/DhLqYNmHd3lRWqkOtbkj9k/gidrotsikly-%2B?node-id=0%3A1&mode=dev
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
---
+++ src/assets/layout/header.tsx
@@ -0,0 +1,16 @@
+
+
+function header() {
+  return (
+    <div className="header-container">
+        <div className="header-left">
+            <h2>Магазины</h2>
+            <h2>Акции</h2>
+            <h2>Доставка и оплата</h2>
+        </div>
+        <div className="header-right"></div>
+    </div>
+  )
+}
+
+export default header
