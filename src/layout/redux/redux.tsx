505dda33b7a8c7d9529f1c8e958ab14ab929f83f
Shakhmurad <muradb356@gmail.com>
2024-02-10 15:28:32 +0400

    add new item in page

--- src/layout/redux/redux.tsx
+++ src/layout/redux/redux.tsx
@@ -1,22 +1,21 @@
 interface RootState {
-  user: any[]; 
-  JetSki: any[];
-  Atv: any[];
+  JetSki: string[] | number[];
+  Atv: string[] | number[];
 }
 
 interface DataAction {
   type: 'JetSki';
-  payload: any[]; 
+  payload: string[] | number[]; 
 }
 
 interface DataActions {
   type: 'Atv'; 
-  payload: any[]; 
+  payload: string[] | number[]; 
 }
 
 type ActionTypes = DataAction | DataActions; 
 
-const RootRedux = (state: RootState = { user: [], JetSki: [], Atv: [] }, action: ActionTypes): RootState => {
+const RootRedux = (state: RootState = {JetSki: [], Atv: [] }, action: ActionTypes): RootState => {
   if (action.type === 'JetSki') {
       return { ...state, JetSki: action.payload };
   } else if (action.type === 'Atv') {
