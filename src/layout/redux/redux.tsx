5902f5e9155becf8f2ea4ab173fcd8bb03ae1a0a
Shakhmurad <muradb356@gmail.com>
2024-02-27 02:48:18 +0400

    add switch in redux

--- src/layout/redux/redux.tsx
+++ src/layout/redux/redux.tsx
@@ -4,6 +4,7 @@ interface RootState {
   Boats: number[] | string[]
   BigBoats: number[] | string[]
   AllTerrainVehicles:number[] | string[]
+  Snowmobiles:number[] | string[]
 }
 
 interface DataAction {
@@ -27,26 +28,47 @@ interface DataBigBoats {
   type: 'BigBoats'; 
   payload:any[]; 
 }
+interface DataSnowmobiles {
+  type: 'Snowmobiles'; 
+  payload:any[]; 
+}
 
-type ActionTypes = DataAction | DataActions | DataBoats | DataBigBoats | DataAllTerrainVehicles;  
+type ActionTypes = DataAction | DataActions | DataBoats | DataBigBoats | DataAllTerrainVehicles |DataSnowmobiles;  
 
-const RootRedux = (state: RootState = {JetSki: [], Atv: [] , Boats:[] , BigBoats:[] , AllTerrainVehicles:[] }, action: ActionTypes): RootState => {
-  if (action.type === 'JetSki') {
+const RootRedux = (state: RootState = {JetSki: [], Atv: [] , Boats:[] , BigBoats:[] , AllTerrainVehicles:[] , Snowmobiles:[] }, action: ActionTypes): RootState => {
+  // if (action.type === 'JetSki') {
+  //     return { ...state, JetSki: action.payload };
+  // } else if (action.type === 'Atv') {
+  //     return { ...state, Atv: action.payload };
+  // }
+  // else if(action.type ==='Boats' ){
+  //   return { ...state, Boats: action.payload };
+  // }
+  // else if(action.type ==='BigBoats' ){
+  //   return { ...state, BigBoats: action.payload };
+  // }
+  // else if(action.type ==='AllTerrainVehicles' ){
+  //   return { ...state, AllTerrainVehicles: action.payload };
+  // }
+  // else if(action.type ==='Snowmobiles' ){
+  //   return { ...state, Snowmobiles: action.payload };
+  // }
+  switch (action.type) {
+    case 'JetSki' :
       return { ...state, JetSki: action.payload };
-  } else if (action.type === 'Atv') {
+    case 'Atv' :
       return { ...state, Atv: action.payload };
-  }
-  else if(action.type ==='Boats' ){
-    return { ...state, Boats: action.payload };
-  }
-  else if(action.type ==='BigBoats' ){
-    return { ...state, BigBoats: action.payload };
-  }
-  else if(action.type ==='AllTerrainVehicles' ){
-    return { ...state, AllTerrainVehicles: action.payload };
-  }
+    case 'Boats' :
+      return { ...state, Boats: action.payload };
+    case 'BigBoats' :
+      return { ...state, BigBoats: action.payload };
+    case 'AllTerrainVehicles' :
+      return { ...state, AllTerrainVehicles: action.payload };
+    case 'Snowmobiles' :
+      return { ...state, Snowmobiles: action.payload };
+    }
 
-  return state;
+    return state;
 };
 
 export default RootRedux;
